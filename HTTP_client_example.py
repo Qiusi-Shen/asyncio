3cac3ebbfbe5d319444e0a053e75b2e82fccc3c8
Qiusi-Shen <qiusi@ucla.edu>
2017-11-18 02:51:34 -0800

    Update HTTP_client_example.py

--- HTTP_client_example.py
+++ HTTP_client_example.py
@@ -1,1 +1,15 @@
+import asyncio
+import aiohttp
 
+async def fetch_page(session, url):
+  with aiohttp.Timeout(10):
+    async with session.get(url) as response:
+      assert reponse.status == 200
+      return await response.read()
+    
+loop = asyncio.get_event_loop()
+with aiohttp.ClientSession(loop=loop) as session:
+  content = loop.run_until_complete(
+    fetch_page(session, 'http://python.org'))
+  print(content)
+loop.close()
